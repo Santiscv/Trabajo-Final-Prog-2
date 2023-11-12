91380021eff8c45f8ce45fdcaf827676b0ec28e7
Santiago <carranzasantiago85@gmail.com>
2023-11-12 12:30:58 -0300

    Se soluciono el inicio, para que pregunte de nuevo si ingresa otro input que no este en la opcion

--- menu_disqueria.py
+++ menu_disqueria.py
@@ -22,128 +22,136 @@ def menu():
 
 def run(disqueria):
     print("Hellfish Disquerias")
-    print("(1) Sí\n(2) No")
-    inicio = input("Elije una opción para entrar al sistema: ")
-    while inicio == '1':
-        opcion = menu()
-        if opcion not in ['1','2','3','4','5','6','7','8','9','10','11','12','13']:
-            print(" ")
+    while True:
+        inicio = input("(1) Sí\n(2) No\nElija una opción para entrar al sistema: ")
+        if inicio == '2':
             print("--")
-            print("Intente nuevamente.")
-            print("Ingrese una opcion valida.")
+            print("Programa finalizado")
             print("--")
-            print(" ")
-            pass
-        while opcion in ['1','2','3','4','5','6','7','8','9','10','11','12','13']:
-            if opcion == "1":
-                # Dar de alta un álbum
-                id_album = input("ID del álbum: ")
-                nombre_album = input("Nombre del álbum: ")
-                artista = input("Nombre del Artista: ")
-                genero_musical = input("Género musical: ")
-                categoria = input("Categoría: ")
-                stock = input("Cantidad de producto: ")
-                precio_de_compra = float(input("Precio de compra: "))
-                precio_venta_cliente = float(input("Precio de venta: "))
-                album = Album(id_album, nombre_album, artista, genero_musical, categoria, stock, precio_de_compra, precio_venta_cliente)
-                if disqueria.contiene_album(album.nombre):
-                    print("El Álbum ya existe.")
-                else:
-                    disqueria.alta_nuevo_album(album)
-                    print("Álbum Agregado")
+            break
+        elif inicio == '1':
+            while True:
+                opcion = menu()
+                if opcion not in ['1','2','3','4','5','6','7','8','9','10','11','12','13']:
+                    print("--")
+                    print("Intente nuevamente.")
+                    print("Ingrese una opcion valida.")
+                    print("--")
+                    continue
 
-            elif opcion == "2":
-                # Dar de baja un álbum
-                id_album = input("Inserte el ID del álbum: ")
-                if disqueria.contiene_album(id_album):
-                    disqueria.eliminar_album(id_album)
-                    print("Álbum dado de baja")
-                else:
-                    print("El álbum no existe")
+                if opcion == "1":
+                    # Dar de alta un álbum
+                    id_album = input("ID del álbum: ")
+                    nombre_album = input("Nombre del álbum: ")
+                    artista = input("Nombre del Artista: ")
+                    genero_musical = input("Género musical: ")
+                    categoria = input("Categoría: ")
+                    stock = input("Cantidad de producto: ")
+                    precio_de_compra = float(input("Precio de compra: "))
+                    precio_venta_cliente = float(input("Precio de venta: "))
+                    album = Album(id_album, nombre_album, artista, genero_musical, categoria, stock, precio_de_compra, precio_venta_cliente)
+                    if disqueria.contiene_album(album.nombre):
+                        print("El Álbum ya existe.")
+                    else:
+                        disqueria.alta_nuevo_album(album)
+                        print("Álbum Agregado")
 
-            elif opcion == "3":
-                # Buscar álbum
-                id_album = input("Inserte el ID del álbum: ")
-                if disqueria.contiene_album(id_album):
-                    disqueria.buscar_album(id_album)
-                else:
-                    print("No se encuentra el álbum")
+                elif opcion == "2":
+                    # Dar de baja un álbum
+                    id_album = input("Inserte el ID del álbum: ")
+                    if disqueria.contiene_album(id_album):
+                        disqueria.eliminar_album(id_album)
+                        print("Álbum dado de baja")
+                    else:
+                        print("El álbum no existe")
 
-            elif opcion == "4":
-                # Modificar álbum
-                id_album = input("Inserte el ID del álbum a modificar: ")
-                if disqueria.contiene_album(id_album):
-                    nuevo_nombre = input("Nuevo nombre del álbum: ")
-                    nuevo_artista = input("Nuevo nombre del artista: ")
-                    nuevo_genero_musical = input("Nuevo género musical: ")
-                    nueva_categoria = input("Nueva categoría: ")
-                    nuevo_stock = input("Nueva cantidad de producto: ")
-                    nuevo_precio_compra = float(input("Nuevo precio de compra: "))
-                    nuevo_precio_venta = float(input("Nuevo precio de venta: "))
-                    album = disqueria.buscar_album(id_album)
-                    album.nombre = nuevo_nombre
-                    album.artista = nuevo_artista
-                    album.genero_musical = nuevo_genero_musical
-                    album.categoria = nueva_categoria
-                    album.stock = nuevo_stock
-                    album.precio_compra_tienda = nuevo_precio_compra
-                    album.precio_venta_cliente = nuevo_precio_venta
-                    print("Álbum modificado con éxito.")
-                else:
-                    print("El álbum no existe en la disquería.")
+                elif opcion == "3":
+                    # Buscar álbum
+                    id_album = input("Inserte el ID del álbum: ")
+                    if disqueria.contiene_album(id_album):
+                        disqueria.buscar_album(id_album)
+                    else:
+                        print("No se encuentra el álbum")
 
-            elif opcion == "5":
-                # Listar álbumes
-                disqueria.mostrar_albumes()
+                elif opcion == "4":
+                    # Modificar álbum
+                    id_album = input("Inserte el ID del álbum a modificar: ")
+                    if disqueria.contiene_album(id_album):
+                        nuevo_nombre = input("Nuevo nombre del álbum: ")
+                        nuevo_artista = input("Nuevo nombre del artista: ")
+                        nuevo_genero_musical = input("Nuevo género musical: ")
+                        nueva_categoria = input("Nueva categoría: ")
+                        nuevo_stock = input("Nueva cantidad de producto: ")
+                        nuevo_precio_compra = float(input("Nuevo precio de compra: "))
+                        nuevo_precio_venta = float(input("Nuevo precio de venta: "))
+                        album = disqueria.buscar_album(id_album)
+                        album.nombre = nuevo_nombre
+                        album.artista = nuevo_artista
+                        album.genero_musical = nuevo_genero_musical
+                        album.categoria = nueva_categoria
+                        album.stock = nuevo_stock
+                        album.precio_compra_tienda = nuevo_precio_compra
+                        album.precio_venta_cliente = nuevo_precio_venta
+                        print("Álbum modificado con éxito.")
+                    else:
+                        print("El álbum no existe en la disquería.")
 
-            elif opcion == "6":
-                # Dar de alta empleado
-                nombre = input("Nombre del empleado: ")
-                dni = input("DNI del empleado: ")
-                telefono = input("Teléfono del empleado: ")
-                direccion = input("Dirección del empleado: ")
-                cargo = input("Cargo asignado: ")
-                empleado = Empleados(nombre, dni, telefono, direccion, cargo)
-                if disqueria.contiene_empleado(empleado.dni):
-                    print("El empleado ya existe.")
-                else:
-                    disqueria.alta_nuevo_empleado(empleado)
-                    print("Empleado dado de alta.")
+                elif opcion == "5":
+                    # Listar álbumes
+                    disqueria.mostrar_albumes()
 
-            elif opcion == "7":
-                # Dar de baja empleado
-                dni_empleado = input("Inserte el DNI del empleado: ")
-                if disqueria.contiene_empleado(dni_empleado):
-                    disqueria.eliminar_empleado(dni_empleado)
-                    print("Empleado dado de baja.")
-                else:
-                    print("El empleado no existe.")
+                elif opcion == "6":
+                    # Dar de alta empleado
+                    nombre = input("Nombre del empleado: ")
+                    dni = input("DNI del empleado: ")
+                    telefono = input("Teléfono del empleado: ")
+                    direccion = input("Dirección del empleado: ")
+                    cargo = input("Cargo asignado: ")
+                    empleado = Empleados(nombre, dni, telefono, direccion, cargo)
+                    if disqueria.contiene_empleado(empleado.dni):
+                        print("El empleado ya existe.")
+                    else:
+                        disqueria.alta_nuevo_empleado(empleado)
+                        print("Empleado dado de alta.")
 
-            elif opcion == "8":
-                pass
-                # Modificar empleado (pendiente)
+                elif opcion == "7":
+                    # Dar de baja empleado
+                    dni_empleado = input("Inserte el DNI del empleado: ")
+                    if disqueria.contiene_empleado(dni_empleado):
+                        disqueria.eliminar_empleado(dni_empleado)
+                        print("Empleado dado de baja.")
+                    else:
+                        print("El empleado no existe.")
 
-            elif opcion == "9":
-                # Buscar empleado
-                dni_empleado = input("Insertar el DNI del empleado: ")
-                if disqueria.contiene_empleado(dni_empleado):
-                    disqueria.buscar_empleado(dni_empleado)
-                else:
-                    print("No se encuentra el empleado")
+                elif opcion == "8":
+                    pass
+                    # Modificar empleado (pendiente)
 
-            elif opcion == "10":
-                # Listar empleados
-                disqueria.mostrar_empleados()
+                elif opcion == "9":
+                    # Buscar empleado
+                    dni_empleado = input("Insertar el DNI del empleado: ")
+                    if disqueria.contiene_empleado(dni_empleado):
+                        disqueria.buscar_empleado(dni_empleado)
+                    else:
+                        print("No se encuentra el empleado")
 
-            elif opcion == "11":
-                # Guardar archivo
-                disqueria.guardar_archivo()
+                elif opcion == "10":
+                    # Listar empleados
+                    disqueria.mostrar_empleados()
 
-            elif opcion == "12":
-                # Leer archivo
-                disqueria.leer_archivo()
+                elif opcion == "11":
+                    # Guardar archivo
+                    disqueria.guardar_archivo()
 
-            opcion = menu()
+                elif opcion == "12":
+                    # Leer archivo
+                    disqueria.leer_archivo()
+
+                elif opcion == "13":
+                    print("--")
+                    print("Saliendo del programa.")
+                    print("--")
+                    break
 
 if __name__ == "__main__":
     disqueria = Disqueria()
