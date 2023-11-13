96d23c164825d272a8c67227f466d02cdc158634
Santiago <carranzasantiago85@gmail.com>
2023-11-13 14:50:46 -0300

    Se agrego el metodo venta_album para poder simular una venta de album

--- disqueria.py
+++ disqueria.py
@@ -2,11 +2,28 @@ import pickle #libreria para guardar y recuperar informacion
 from Arbol_binario import ArbolBinarioBusqueda #abb = Arbol binario de busqueda.
 
 class Disqueria:
-    def __init__(self, dinero_invertido):
+    def __init__(self):
         self.arbol_album = ArbolBinarioBusqueda()
         self.arbol_empleado = ArbolBinarioBusqueda()
-        self.presupuesto_disqueria = dinero_invertido
-    
+        self.presupuesto_disqueria = 0
+
+    def venta_album(self, id_album, cantidad, dni_empleado):
+        album = self.buscar_album2(id_album)
+        empleado = self.buscar_empleado2(dni_empleado)
+
+        if album and empleado and cantidad > 0 and album.stock >= cantidad:
+            # Realizar la venta
+            precio_venta_total = album.precio_venta_cliente * cantidad
+
+            # Actualizar empleado
+            empleado.dinero_aportado += precio_venta_total
+
+            # Actualizar album
+            album.dinero_en_caja += precio_venta_total
+            album.stock -= cantidad
+            
+ 
+
     def __len__(self):
         return len(self.arbol_album)
     
@@ -21,10 +38,10 @@ class Disqueria:
         else:
             return None
         
-    def buscar_album2(self,id)->"Album": #igual
+    def buscar_album2(self,id)->"Album": #IGUAL QUE BUSCAR ALBUM PERO SIN PRINT
+        id= str(id)
         if id in self.arbol_album:
             album = self.arbol_album[id]
-        
             return album
         else:
             return None
@@ -39,7 +56,8 @@ class Disqueria:
     
     
     def mostrar_albumes(self):
-        self.arbol_album.inorden() #provisorio
+        self.arbol_album.EnOrden(self.arbol_album.raiz)
+    
             
             #probar
     def modificar_album(self, id,nuevo_nombre, nuevo_artista, nuevo_genero, nueva_categoria, nuevo_stock):
@@ -56,39 +74,50 @@ class Disqueria:
         
     ############ empleado ##########
     
+    def __len_empleado__(self):
+        return len(self.arbol_empleado)
     
     def alta_nuevo_empleado(self, empleado)->None: #igual
         self.arbol_empleado[empleado.dni] = empleado
         
     
-    def eliminar_empleado(self,dni)->None:
+    def eliminar_empleado(self, dni)->None:
         self.arbol_empleado.eliminar(dni) 
-        #empleado = self.buscar_empleado(dni)
-        #self.arbol_empleado.eliminar(empleado)   
+
+          
                     
     def contiene_empleado(self,dni)->bool: #igual
         return dni in self.arbol_empleado
     
+    
     def buscar_empleado(self, dni)->"Empleados": #igual
         if dni in self.arbol_empleado:
-            empleado= self.arbol_empleado[dni]
+            empleado = self.arbol_empleado[dni]
             print(empleado)
             return empleado
         else:
             return None
-    
+
+    def buscar_empleado2(self, dni)->"Empleados": #IGUAL QUE BUSCAR EMPLEADO PERO SIN PRINT
+        if dni in self.arbol_empleado:
+            empleado= self.arbol_empleado.obtener(dni)
+            #empleado= self.arbol_empleado[dni]
+            return empleado
+        else:
+            return None    
     
     def mostrar_empleados(self):
-        self.arbol_empleado.inorden() #provisorio
+        self.arbol_empleado.EnOrden_empleado(self.arbol_empleado.raiz)
+        #self.arbol_empleado.inorden() #provisorio
     
         
     def modificar_empleado(self, dni, nuevo_nombre,nuevo_telefono, nueva_direccion, nuevo_cargo, nuevo_dinero_aportado):
-        if self.arbol_empleado[dni] == self.arbol_empleado.buscar_empleado(dni):
+        if self.arbol_empleado[dni] == self.arbol_empleado.buscar_empleado2(dni):
             self.arbol_empleado.nombre = nuevo_nombre
             self.arbol_empleado.telefono = nuevo_telefono
             self.arbol_empleado.direccion = nueva_direccion
             self.arbol_empleado.cargo = nuevo_cargo
-            self.arbol_empleado.dinero_aportado = nuevo_dinero_aportado
+            #self.arbol_empleado.dinero_aportado = nuevo_dinero_aportado
 
 
          
@@ -100,8 +129,8 @@ class Disqueria:
     def leer_archivo(self,archivo="disqueria.pickle"):
         pickle_file = open(archivo,'rb')
         disqueria = pickle.load(pickle_file)
-        self.albumes = disqueria.albumes
-        self.empleados = disqueria.empleados
+        self.arbol_album = disqueria.arbol_album
+        self.arbol_empleado = disqueria.arbol_empleado
         pickle_file.close()
 
 class Album:
@@ -114,7 +143,7 @@ class Album:
         self.stock = stock
         self.precio_compra_tienda = precio_compra
         self.precio_venta_cliente = precio_venta
-        self.dinero_en_caja = precio_venta - precio_compra
+        self.dinero_beneficio = precio_venta - precio_compra
 
     def __lt__(self, other): # x<y llama x.__lt__(y)
         return self.nombre<other.nombre
@@ -129,8 +158,7 @@ class Album:
     def __ge__(self, other): # x>=y llama x.__ge__(y)
         return self.nombre>=other.nombre
           
-        
-    
+           
 
     def __str__(self):
         return "ID: {0}\nAlbum: {1}\nArtista: {2}\nGenero Musical: {3}\nCategoria:  {4}\nStock:  {5}\nPrecio para la tienda: {6}\nPrecio para el cliente: {7}\nDinero en caja: {8}" \
@@ -159,12 +187,9 @@ class Empleados:
     def __ge__(self, other): # x>=y llama x.__ge__(y)
         return self.dni>=other.dni
 
-    def __len__(self):
-        return len(self.arbol_empleado)
+    #def __len__(self):
+    #    return len(self.arbol_empleado)
     
     def __str__(self):
         return "nombre: {0}\nDNI: {1}\nTelefono: {2}\nDireccion: {3}\nCargo: {4}\nDinero aportado: {5}" \
             .format(self.nombre, self.dni, self.telefono,self.direccion,self.cargo, self.dinero_aportado)
-
-
-
